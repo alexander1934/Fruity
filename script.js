0b7138e2b67ab3db3e8e9392787f9bea461a3491
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-10-13 15:59:10 +0300

    Added script for rewiews

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const rewiew = document.querySelector('.rewiew-div');
-const rewiewText = rewiew.textContent
-// console.log(rewiewText)
+let newButton = document.querySelector('.button');
+let reverseButton = document.querySelector('.reverse-button');
+let rewiew = document.querySelector('.rewiew-div');
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+newButton.onclick = ((currentPosition = 0) => () => {
+    if (currentPosition < arr.length) {
+      rewiew.textContent = arr[currentPosition++]
+      console.log(currentPosition)
+    };
+  })();
 
-// const array = ['frgergbergr', 'rtrggrghr']
-//  function arrayCycle (array){
-//     let i
-//     for(i=0; i<array.length; i++)
-//    console.log(array[i])
-// }
-
-const textValue = () => {
-    rewiew.textContent = "rgreugbnrg"
-}
-
-const button = document.querySelector('.button')
-
-button.addEventListener("click", textValue)
-
-// // arrayCycle(array)
+  reverseButton.onclick = ((currentPosition) => () => {
+    if (currentPosition < arr.length) {
+      rewiew.textContent = arr[currentPosition--]
+      console.log(currentPosition)
+    };
+  })();
